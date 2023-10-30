a30c2050d5614d7dddeea88d083a0872f04d1738
Andrew Paglusch <AndrewPaglusch@users.noreply.github.com>
2023-10-30 15:46:38 -0500

    complete rewrite. use exceptions

--- gps.py
+++ gps.py
@@ -11,123 +11,118 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 
+class SIM7080GException(Exception):
+    pass
+
+
 class SIM7080G:
     def __init__(self):
-        self.ser = serial.Serial("/dev/ttyUSB2",
-                                 baudrate=115200,
-                                 exclusive=True,
-                                 rtscts=True,
-                                 dsrdtr=True)
-        self.ser.reset_input_buffer()
-        self.ser.reset_output_buffer()
+        self.serial_port = serial.Serial("/dev/ttyUSB2",
+                                         baudrate=115200,
+                                         exclusive=True,
+                                         rtscts=True,
+                                         dsrdtr=True)
+        self.serial_port.reset_input_buffer()
+        self.serial_port.reset_output_buffer()
 
-    def _send_at_str(self,
-                     command,
-                     expected_reply_regex="^OK$",
-                     failure_reply_regex="^ERROR.*",
-                     regex_return_filter=".*"):
+    def _write_serial_data(self, data_str):
+        try:
+            logger.debug(f"Sending data to serial interface: \"{data_str}\"")
+            self.serial_port.write((data_str + "\r\n").encode("utf-8"))
+            time.sleep(1)  # give time for data to be sent
+        except serial.SerialTimeoutException:
+            raise SIM7080GException("Write timeout occurred while sending data to serial interface.")
+        except serial.SerialException as serial_exception:
+            raise SIM7080GException(f"Serial exception occurred: {serial_exception}")
+        except Exception as general_exception:
+            raise SIM7080GException(f"An unexpected error occurred: {general_exception}")
 
-        # retry reading response to AT command this many times for each send attempt
+    def _read_serial_data(self):
         read_retry_limit = 3
 
-        logger.debug(f"Sending data to serial interface: \"{command}\"")
-        self.ser.write((command + "\r\n").encode("utf-8"))
-        time.sleep(3)
-
         for read_attempt in range(read_retry_limit):
-            logger.debug(f"Starting read attempt {read_attempt +1} of {read_retry_limit}...")
+            logger.debug(f"Starting read attempt {read_attempt + 1} of {read_retry_limit}...")
 
-            # is there data waiting?
-            if not self.ser.in_waiting:
+            if not self.serial_port.in_waiting:
                 logger.warning("No response data waiting. Will try reading again in 3 seconds...")
                 time.sleep(3)
                 continue
 
-            logger.debug(f"Found {self.ser.in_waiting} bytes waiting on serial output")
+            logger.debug(f"Found {self.serial_port.in_waiting} bytes waiting on serial output")
 
-            # read response as-is
-            response_data = self.ser.read(self.ser.in_waiting).decode()
-
+            response_data = self.serial_port.read(self.serial_port.in_waiting).decode()
             logger.debug(f"AT command response data (untouched):\n---- BEGIN ----\n{response_data}\n---- END ----")
-            
-            # remove \r chars from reply, keeping only \n
-            response_data = response_data.replace("\r", "")
-            
-            # remove our command from the response if its there
-            response_data = re.sub(f"^{re.escape(command)}$", "", response_data, flags=re.MULTILINE)
 
-            logger.debug(f"AT command response data (after inital cleanup):\n---- BEGIN ----\n{response_data}\n---- END ----")
+            response_data = response_data.replace("\r", "")  # Remove \r characters, keep \n characters
 
-            # check to see if reply matches success regex
-            if re.search(expected_reply_regex, response_data, re.MULTILINE):
-                logger.debug(f"Response matches expected reply regex of \"{expected_reply_regex}\". Returning data filtered with regex \"{regex_return_filter}\"")
+            return response_data
 
-                regex_filtered_response = "".join(re.findall(regex_return_filter, response_data, re.MULTILINE))
-                logger.debug(f"AT command response data (after regex filter):\n---- BEGIN ----\n{regex_filtered_response}\n---- END ----")
+        raise SIM7080GException("Unable to read any data from serial device.")
 
-                # only return data that matches the given regex
-                return True, regex_filtered_response
+    def _clean_serial_response(self, input_data, line_to_remove):
+        return re.sub(f"^{re.escape(line_to_remove)}$", "", input_data, flags=re.MULTILINE)
 
-            # check to see if reply matches failure regex
-            if re.search(failure_reply_regex, response_data, re.MULTILINE):
-                logger.error(f"Response matches failure reply regex of \"{failure_reply_regex}\". Returning data filtered with fail regex.")
+    def _send_at_command(self, *, command, expected_reply_regex="^OK$", failure_reply_regex="^ERROR.*", regex_return_filter=".*"):
+        self._write_serial_data(command)
 
-                regex_filtered_response = "".join(re.findall(failure_reply_regex, response_data, re.MULTILINE))
-                logger.debug(f"AT command failure response data (after regex filter):\n---- BEGIN ----\n{regex_filtered_response}\n---- END ----")
+        response_data = self._read_serial_data()
+        response_data = self._clean_serial_response(response_data, command)
 
-                # only return data that matches the failure regex
-                return False, regex_filtered_response
+        logger.debug(f"AT command response data (after initial cleanup):\n---- BEGIN ----\n{response_data}\n---- END ----")
 
-            # we got data, but it didnt match the success or fail regex
-            # sleep a try again
-            logger.warning("Response data does not match success or failure regex. Will try reading again in 3 seconds...")
-            time.sleep(3)
+        if re.search(expected_reply_regex, response_data, re.MULTILINE):
+            logger.debug(f"Response matches expected reply regex: \"{expected_reply_regex}\". Returning data filtered with regex: \"{regex_return_filter}\".")
 
-        return False, f"Failed to get a response that matches success or fail regex after {read_retry_limit} read attempts."
+            filtered_response = "".join(re.findall(regex_return_filter, response_data, re.MULTILINE))
+            logger.debug(f"AT command response data (after regex filter):\n---- BEGIN ----\n{filtered_response}\n---- END ----")
+
+            return filtered_response
+
+        if re.search(failure_reply_regex, response_data, re.MULTILINE):
+            logger.debug(f"Response matches failure reply regex: \"{failure_reply_regex}\". Returning data filtered with failure regex.")
+
+            filtered_response = "".join(re.findall(failure_reply_regex, response_data, re.MULTILINE))
+            logger.debug(f"AT command failure response data (after regex filter):\n---- BEGIN ----\n{filtered_response}\n---- END ----")
+
+            raise SIM7080GException(filtered_response)
+
+        logger.error(f"Response data does not match success or failure regex.")
+        raise SIM7080GException("Response data does not match success or failure regex.")
 
     def gps_power_on(self):
         logger.info("Powering on GPS modem...")
-        success, msg = self._send_at_str("AT+CGNSPWR=1")
-        if success:
-            logger.info("GPS power on success")
-        else:
-            logger.info(f"GPS power on failed: {msg}")
+        try:
+            self._send_at_command(command="AT+CGNSPWR=1")
+            logger.info("GPS power on successful.")
+        except SIM7080GException as e:
+            logger.info(f"GPS power on failed: {e}")
 
     def gps_power_off(self):
         logger.info("Powering off GPS modem...")
-        success, msg = self._send_at_str("AT+CGNSPWR=1")
-        if success:
+        try:
+            self._send_at_command(command="AT+CGNSPWR=0")
             logger.info("GPS power off success")
-        else:
-            logger.info(f"GPS power off failed: {msg}")
+        except SIM7080GException as e:
+            logger.info(f"GPS power off failed: {e}")
 
     def get_gps_position(self):
         while True:
             logger.info("Requesting GPS information...")
-            success, msg = self._send_at_str("AT+CGNSINF", expected_reply_regex=".*\+CGNSINF:.*", regex_return_filter="^\+CGNSINF.*")
-
-            if success:
-                if ",,,," in msg:
+            try:
+                response = self._send_at_command(command="AT+CGNSINF", expected_reply_regex=".*\+CGNSINF:.*", regex_return_filter="^\+CGNSINF.*")
+                if ",,,," in response:
                     logger.info("Waiting for GPS lock. Retrying in 10 seconds...")
                 else:
-                    logger.info(f"Got a valid GPS location. Parsing...")
-                    logger.debug(f"Sending GPS response to parser: {msg}")
-                    self._parse_gps_info(msg)
+                    logger.info("Got a valid GPS location. Parsing...")
+                    logger.debug(f"Sending GPS response to parser: {response}")
+                    self._parse_gps_info(response)
                     break
-            else:
-                logger.warning(f"Failed reading from GPS. Retrying in 10 seconds. Error: {msg}")
+            except SIM7080GException as e:
+                logger.warning(f"Failed reading from GPS. Retrying in 10 seconds. Error: {e}")
 
             time.sleep(10)
 
     def _parse_gps_info(self, gps_data):
-        """
-        The GPS sends us back a bunch of values seperated with ",".
-        We got the key names from the docs and the type they're supposed to be.
-        This function associates the values with the correct key name and also
-        casts the values to the correct type
-        """
-
-        # names and types from docs
         gnss_key_names_and_expected_types = {
             "gnss_run_state": int,
             "gps_fix_status": int,
@@ -151,60 +146,100 @@ class SIM7080G:
 
         values_from_modem = gps_data.split(": ")[1].split(',')
 
-        # merge the values from the gps with the key names
         gps_json = dict(zip(gnss_key_names_and_expected_types.keys(), values_from_modem))
 
-        # cast values to correct type
         for gnss_key_name, item_value in gps_json.items():
             if item_value:
                 expected_item_type = gnss_key_names_and_expected_types[gnss_key_name]
                 logging.debug(f"Trying to cast {gnss_key_name} field's value of \"{item_value}\" to type {expected_item_type}...")
                 gps_json[gnss_key_name] = expected_item_type(item_value)
 
-        print(json.dumps(gps_json, indent=3))
-        print(f"https://www.google.com/maps/search/?api=1&query={gps_json['latitude']},{gps_json['longitude']}")
+        return gps_json
+
+    def _is_network_up(self):
+        logger.debug("Checking to see if network is up...")
+
+        success_regex = r'^\+CNACT: 0,1,"(?!0\.0\.0\.0")\d+\.\d+\.\d+\.\d+"$'
+        fail_regex = r'^\+CNACT: 0,0,"0\.0\.0\.0"$'
+        try:
+            self._send_at_command(command='AT+CNACT?',
+                                  expected_reply_regex=success_regex,
+                                  failure_reply_regex=fail_regex)
+            return True
+        except SIM7080GException:
+            return False
+
+    def activate_network(self):
+        logger.info("Activating network...")
+        if self._is_network_up():
+            logger.info("Network is up")
+            return True
+        else:
+            try:
+                self._send_at_command(command='AT+CNACT=0,1', expected_reply_regex=r'^OK$')
+                logger.info("Network activation successful.")
+                return True
+            except SIM7080GException as e:
+                logger.error(f"Failed to activate network. Error: {e}")
+                return False
+
+    def deactivate_network(self):
+        logger.info("Deactivating network...")
+        if not self._is_network_up():
+            logger.info("Network is down")
+        else:
+            try:
+                self._send_at_command(command='AT+CNACT=0,0', expected_reply_regex=r'^OK$')
+                logger.info("Network deactivation successful.")
+            except SIM7080GException as e:
+                logger.error(f"Failed to deactivate network. Error: {e}")
 
     def post_json_payload(self, url, json_string):
-        # Setup the HTTPS parameters
-        self._send_at_str('AT+SHCONF="bodytx",1')
-        self._send_at_str('AT+SHCONF="contype","application/json"')
 
-        # Start HTTPS session
-        self._send_at_str('AT+SHCONN')
+        logger.info("Preparing modem of HTTPS JSON POST...")
+        ok_regex = '^OK$'
+        commands = (
+            ('AT+CSSLCFG="sslversion",1,3', ok_regex, 'Setting TLS version'),
+            ('AT+CSSLCFG="ignorertctime",1,1', ok_regex, 'Disabling TLS cert expiration checking'),
+            ('AT+SHSSL=1,""', ok_regex, 'Relaxing TLS verification'),
+            ('AT+CSSLCFG="sni",1,"paglusch.com"', ok_regex, 'Enabling SNI'),
+            (f'AT+SHCONF="URL","{url}"', ok_regex, f'Setting URL to {url}'),
+            ('AT+SHCONF="BODYLEN",1024', ok_regex, 'Setting max body size to 1024'),
+            ('AT+SHCONF="HEADERLEN",350', ok_regex, 'Setting max header size to 350')
 
-        # Send the POST request
-        self._send_at_str(f'AT+SHREQ="{url}",4,{len(json_string)}')
-        self._send_at_str(json_string, expected_reply_regex="^>", regex_return_filter=".*")
-
-        # End HTTPS session
-        self._send_at_str('AT+SHDISC')
-
-    def test_modem(self):
-        logging.info("Checking if SIM7080X is ready...")
-        while True:
-            if self._send_at_str("AT"):
-                logger.info("SIM7080X apears to be ready and responsive")
+        )
+        for command, expected_reply_regex, status_msg in commands:
+            logger.info(f"POST : {status_msg}...")
+            try:
+                self._send_at_command(command=command,
+                                      expected_reply_regex=expected_reply_regex)
+            except SIM7080GException as e:
+                logger.error(f"Failed during JSON POST on command \"{command}\" ({status_msg}). Error: {e}")
                 return
-            logger.info("Did not get an \"OK\" back from modem. Testing again...")
+
+        print("Done with post")
 
 
 def main():
     logging.basicConfig(
         stream=sys.stderr,
-        # level=logging.INFO,
-        level=logging.DEBUG,
+        level=logging.INFO,
+        # level=logging.DEBUG,
         format='%(asctime)s - %(levelname)s: %(message)s',
         datefmt='%Y-%m-%d %H:%M:%S'
     )
 
-    modem = SIM7080G()
+    sim7080g = SIM7080G()
+    try:
+        sim7080g.gps_power_on()
+        gps_json = sim7080g.get_gps_position()
+        sim7080g.gps_power_off()
 
-    modem.test_modem()
-    modem.gps_power_on()
-    modem.get_gps_position()
-    modem.gps_power_off()
-
-    # modem.post_json_payload('https://paglusch.com', '{"key": "value"}')
+        if sim7080g.activate_network():
+            sim7080g.post_json_payload('https://paglusch.com/', gps_json)
+            sim7080g.deactivate_network()
+    finally:
+        sim7080g.serial_port.close()
 
 
 if __name__ == "__main__":
